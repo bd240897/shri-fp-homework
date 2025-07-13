2c38ff865a9dfb1da067ab8a566ef34a1a110496
bd240897 <bd240897@yandex.ru>
2025-07-13 23:52:20 +0300

    feat: add v1 of both tasks

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,5 @@
+import * as R from "ramda";
+
 /**
  * @file Домашка по FP ч. 2
  *
@@ -14,38 +16,88 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+/**
+ * Я – пример, удали меня
+ */
+const wait = (time) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const processSequence = async ({
+  value,
+  writeLog,
+  handleSuccess,
+  handleError,
+}) => {
+  const validatedNumber = R.pipe(
+    // 1. Берем строку N. Пишем изначальную строку в writeLog
+    R.tap(writeLog),
+    // 2. Строка валидируется по следующим правилам:
+    // кол-во символов в числе должно быть меньше 10.
+    // кол-во символов в числе должно быть больше 2.
+    // число должно быть положительным
+    // символы в строке только [0-9] и точка т.е. число в 10-ной системе счисления (возможно с плавающей запятой)
+    // В случае ошибки валидации вызвать handleError с 'ValidationError' строкой в качестве аргумента
+    R.tap(
+      R.ifElse(
+        R.pipe(
+          // R.prop(value),
+          R.allPass([
+            // Длина от 2 до 10 символов
+            R.pipe(
+              String,
+              R.prop("length"),
+              R.both(R.gte(R.__, 2), R.lte(R.__, 10))
+            ),
+            R.pipe(Number, R.gt(R.__, 0)),
+            R.test(/^[0-9.]+$/),
+          ])
+        ),
+        // Если валидация успешна
+        handleSuccess,
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+        // Если валидация не прошла
+        () => handleError("ValidationError")
+      )
+    ),
+    // 3. Привести строку к числу, округлить к ближайшему целому с точностью до единицы, записать в writeLog.
+    R.tap(console.log),
+    Number,
+    Math.round,
+    R.tap(writeLog)
+  )(value);
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+  // 4. C помощью API /numbers/base перевести из 10-й системы счисления в двоичную, результат записать в writeLog
+  const { result: binary } = await api.get("https://api.tech/numbers/base", {
+    from: 10,
+    to: 2,
+    number: validatedNumber.toString(),
+  });
+  writeLog(binary);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+  const processBinary = R.pipe(
+    R.prop("length"),
+    R.tap((len) => {
+      // Взять кол-во символов в полученном от API числе записать в writeLog
+      writeLog(len); 
+      // Возвести в квадрат с помощью Javascript записать в writeLog
+      writeLog(len ** 2); 
+    }),
+    // Взять остаток от деления на 3, записать в writeLog
+    (len) => len ** 2 % 3, // Остаток от деления
+    R.tap(writeLog)
+  )({ length: binary.length });
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  // C помощью API /animals.tech/id/name получить случайное животное используя полученный остаток в качестве id
+  const { name: animal } = await api.get(`/animals/tech/id/${processBinary}`);
+  
+  // Завершить цепочку вызовом handleSuccess в который в качестве аргумента положить результат полученный на предыдущем шаге
+  handleSuccess(animal);
+};
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,6 @@
+import * as R from "ramda";
+import { COLORS, SHAPES } from "../constants";
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -14,37 +17,112 @@
  */
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = (figures) => {
+  const isTriangleWhite = R.propEq(SHAPES.TRIANGLE, COLORS.WHITE);
+  const isCircleWhite = R.propEq(SHAPES.CIRCLE, COLORS.WHITE);
+  const isStarRed = R.propEq(SHAPES.STAR, COLORS.RED);
+  const isSquareGreen = R.propEq(SHAPES.SQUARE, COLORS.GREEN);
 
-    return star === 'red' && square === 'green';
+  console.log(figures);
+  console.log("Triangle is white:", isTriangleWhite(figures));
+  console.log("Circle is white:", isCircleWhite(figures));
+  console.log("Star is red:", isStarRed(figures));
+  console.log("Square is green:", isSquareGreen(figures));
+
+  return R.allPass([isTriangleWhite, isCircleWhite, isStarRed, isSquareGreen])(
+    figures
+  );
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (figures) => {
+  const compare = R.pipe(
+    R.values,
+    R.filter(R.equals(COLORS.GREEN)),
+    R.length,
+    R.gte(R.__, 2)
+  );
+  return compare(figures);
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (figures) => {
+  // объединяющая функция
+  return R.converge(R.equals, [
+    R.pipe(R.values, R.filter(R.equals(COLORS.RED)), R.length),
+    R.pipe(R.values, R.filter(R.equals(COLORS.BLUE)), R.length),
+  ])(figures);
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+
+export const validateFieldN4 = (figures) => {
+  return R.allPass([
+    R.propEq(SHAPES.CIRCLE, COLORS.BLUE),
+    R.propEq(SHAPES.STAR, COLORS.RED),
+    R.propEq(SHAPES.SQUARE, COLORS.ORANGE),
+  ])(figures);
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (figures) => {
+  return R.pipe(
+    R.values,
+    R.filter(R.complement(R.equals(COLORS.WHITE))),
+    R.countBy(R.identity),
+    R.values,
+    R.any(R.gte(R.__, 3))
+  )(figures);
+};
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (figures) => {
+  return R.allPass([
+    // Ровно 2 зелёные фигуры
+    R.pipe(R.values, R.filter(R.equals(COLORS.GREEN)), R.length, R.equals(2)),
+
+    // Ровно 1 красная фигура
+    R.pipe(R.values, R.filter(R.equals(COLORS.RED)), R.length, R.equals(1)),
+
+    // Треугольник зелёный
+    R.propEq(SHAPES.TRIANGLE, COLORS.GREEN),
+  ])(figures);
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (figures) => {
+  return R.pipe(
+    R.values,
+    R.filter(R.equals(COLORS.ORANGE)),
+    R.length,
+    R.equals(R.__, 4)
+  )(figures);
+};
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = (figures) => {
+  const starColor = R.prop(SHAPES.STAR, figures);
+  return R.allPass([
+    R.complement(R.equals(COLORS.WHITE)),
+    R.complement(R.equals(COLORS.RED)),
+  ])(starColor);
+};
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (figures) => {
+  return R.pipe(
+    R.values,
+    R.filter(R.equals(COLORS.GREEN)),
+    R.length,
+    R.equals(R.__, 4)
+  )(figures);
+};
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = (figures) => {
+  return R.allPass([
+    R.converge(R.equals, [R.prop(SHAPES.TRIANGLE), R.prop(SHAPES.SQUARE)]),
+    R.complement(R.propEq(SHAPES.TRIANGLE, COLORS.WHITE)),
+    R.complement(R.propEq(SHAPES.SQUARE, COLORS.WHITE)),
+  ])(figures)
+};
